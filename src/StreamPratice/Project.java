ec43bb5356f234b66b38d4844b6b80ced120b7ba
leo <leo09613257@gmail.com>
2021-04-19 21:51:08 +0800

    123

--- src/StreamPratice/Project.java
+++ src/StreamPratice/Project.java
@@ -6,60 +6,63 @@ import lombok.Data;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 要去plugin 下載 lombok 才能dependency
+ */
 
-@Data
-@Builder
-public class Project {
+    @Data
+    @Builder
+    public  class Project {
 
-    /**
-     * 项目名称
-     */
-    private String  name;
+        /**
+         * 项目名称
+         */
+        private String name;
 
-    /**
-     * 编程语言
-     */
-    private String  language;
+        /**
+         * 编程语言
+         */
+        private String language;
 
-    /**
-     * star 数
-     */
-    private Integer stars;
+        /**
+         * star 数
+         */
+        private Integer stars;
 
-    /**
-     * 描述
-     */
-    private String  description;
+        /**
+         * 描述
+         */
+        private String description;
 
-    /**
-     * 作者
-     */
-    private String  author;
+        /**
+         * 作者
+         */
+        private String author;
 
-    /**
-     * fork数
-     */
-    private Integer forks;
+        /**
+         * fork数
+         */
+        private Integer forks;
 
-    public static List<Project> buildData(){
-        List<Project> data = new ArrayList<>();
+        public static List<Project> buildData() {
+            List<Project> data = new ArrayList<>();
 
-        data.add(Project.builder().name("Blade").language("java").author("biezhi")
-                .stars(3500).forks(2000).description("Lightning fast and elegant mvc framework for Java8").build());
+            data.add(Project.builder().name("Blade").language("java").author("biezhi")
+                    .stars(3500).forks(2000).description("Lightning fast and elegant mvc framework for Java8").build());
 
-        data.add(Project.builder().name("Tale").language("javascript").author("biezhi")
-                .stars(2600).forks(2300).description("Best beautiful java blog, worth a try").build());
+            data.add(Project.builder().name("Tale").language("javascript").author("biezhi")
+                    .stars(2600).forks(2300).description("Best beautiful java blog, worth a try").build());
 
-        data.add(Project.builder().name("Vue.js").language("js").author("yyx990803")
-                .stars(83000).forks(10322).description("A progressive, incrementally-adoptable JavaScript framework for building UI on the web.").build());
+            data.add(Project.builder().name("Vue.js").language("js").author("yyx990803")
+                    .stars(83000).forks(10322).description("A progressive, incrementally-adoptable JavaScript framework for building UI on the web.").build());
 
-        data.add(Project.builder().name("Flask").language("python").author("pallets")
-                .stars(10500).forks(3000).description("The Python micro framework for building web applications").build());
+            data.add(Project.builder().name("Flask").language("python").author("pallets")
+                    .stars(10500).forks(3000).description("The Python micro framework for building web applications").build());
 
-        data.add(Project.builder().name("Elves").language("java").author("biezhi")
-                .stars(200).forks(100).description("Spider").build());
+            data.add(Project.builder().name("Elves").language("java").author("biezhi")
+                    .stars(200).forks(100).description("Spider").build());
 
-        return data;
+            return data;
+        }
     }
 
-}
