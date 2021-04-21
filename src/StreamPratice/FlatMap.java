291080838c4c58858b1410c3e82170b61fc0732b
leo0961325 <47008876+leo0961325@users.noreply.github.com>
2021-04-21 18:41:45 +0800

    FlatMap feat.

--- src/StreamPratice/FlatMap.java
+++ src/StreamPratice/FlatMap.java
@@ -8,6 +8,7 @@ public class FlatMap {
 
     /**
      * FlatMap 扁平化Stream 合併起來，扁平化成一個流
+     *
      * 此時將一個大流包含一個個小流，須將小流合併成一個流
      * 將小流合併成一個大流 : 用flatMap
      * @param args
@@ -17,13 +18,18 @@ public class FlatMap {
         List<String> list = Arrays.asList("I am boy " , "She is a girl" , "But the Girl loves another Girl");
 
         list.stream()
-                .map(m -> m.split("")) //分成三個流 但都還是Stream<String[]> 還是一個集合，所以打印會是亂數(沒有toString)
+                .map(m -> m.split("")) //分成三個流 但都還是Stream<String[]>(如下) 還是一個集合，所以打印會是亂數(沒有toString)
                 .flatMap(Arrays::stream)    //合併成一個流
                 .collect(Collectors.toList())
                 .forEach(s -> System.out.println(s));
 
-        }
 
+        list.stream()
+                .map(m -> m.split(""))
+                .map(s -> s.length)
+                .collect(Collectors.toList())
+                .forEach(strings -> System.out.println(strings));
+    }
 
 
 
